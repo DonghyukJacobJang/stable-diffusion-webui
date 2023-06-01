362c66116e725155db995cc30e5ceb51e88d1a58
Donghyuk Jang <donghyuk0117@gmail.com>
2023-06-01 17:31:46 -0400

    update jupyter script

--- setup-jupyter.sh
+++ setup-jupyter.sh
@@ -1,6 +1,6 @@
 # jupyter
 python3 -m venv pyenv
 source pyenv/bin/activate
-pip3 install jupyterlab
+pip3 install jupyterlab jupyter
 jupyter notebook --generate-config --JupyterApp.answer_yes=True
 jupyter-lab --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.password="$(echo password | python -c 'from notebook.auth import passwd;print(passwd(input()))')"
